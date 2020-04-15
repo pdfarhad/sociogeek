5cfd887508ac2680044508342f8b8c3cde7f9d15
farhad alam <farhad.alam@shohoz.com>
2020-04-16 03:31:26 +0600

    firebase auth added

--- sociogeek-functions/functions/index.js
+++ sociogeek-functions/functions/index.js
@@ -1,38 +1,84 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const express = require('express');
+const app = express();
+const firestoreService = require('firestore-export-import');
+const firebase = require('firebase');
+var serviceAccount = require('../../../sociogeek/sociogeek-1cfbb-firebase-adminsdk.json');
+
+const databaseURL = 'https://sociogeek-1cfbb.firebaseio.com';
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: databaseURL
+});
+
+const db = admin.firestore();
 // admin.initializeApp();
-admin.initializeApp();
+var firebaseConfig = {
+    apiKey: "AIzaSyDdlZ6UkFvyHus_uwmWQvuGM8JANUpu40A",
+    authDomain: "sociogeek-1cfbb.firebaseapp.com",
+    databaseURL: "https://sociogeek-1cfbb.firebaseio.com",
+    projectId: "sociogeek-1cfbb",
+    storageBucket: "sociogeek-1cfbb.appspot.com",
+    messagingSenderId: "217905569579",
+    appId: "1:217905569579:web:1fe08c3f9be1466a64af6e",
+    measurementId: "G-3D59C5MFVR"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firestoreService.initializeApp(serviceAccount, databaseURL);
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-exports.getGeekShots = functions.https.onRequest((request, response) => {
-    admin.firestore().collection("geekshots").get()
+app.get('/geekshots', (request, response) => {
+    db
+        .collection("geekshots")
+        .orderBy("createdAt", "desc")
+        .get()
         .then(data => {
             let shots = [];
             data.forEach(doc => {
-                shots.push(doc.data());
+                shots.push({
+                    shotId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                    // ...doc.data()
+                });//doc.data()
             });
             return response.json(shots);
         })
         .catch(err => console.log(err));
 });
-exports.createGeetShots = functions.https.onRequest((request, response)=>{
 
-    if(request.method !== 'POST'){
-        return response.status(400).json({error: "Method not allowed"});
+// JSON To Firestore
+const jsonToFirestore = async () => {
+    try {
+      console.log('Initialzing Firebase');
+      console.log('Firebase Initialized');
+  
+      await firestoreService.restore('../pentesters.json');
+      console.log('Upload Success');
     }
+    catch (error) {
+      console.log(error);
+    }
+  };
+  
+app.get('/upload-pentesters',( request, response) =>{
+    jsonToFirestore();
+})
+app.post( '/geekshot',(request, response)=>{
+
+    // if(request.method !== 'POST'){
+    //     return response.status(400).json({error: "Method not allowed"});
+    // }
     const newGeekShots = {
         body: request.body.body,
         userHandle: request.body.userHandle,
-        createdAt:admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
-    admin
-        .firestore()
+    db
         .collection("geekshots")
         .add(newGeekShots)
         .then((doc) =>{
@@ -44,3 +90,30 @@ exports.createGeetShots = functions.https.onRequest((request, response)=>{
         });
 
 });
+
+// Signup 
+
+app.post('/signup', (request, response) =>{
+
+    const newUser = {
+        email : request.body.email,
+        password : request.body.password,
+        confirmPassword : request.body.confirmPassword,
+        handle : request.body.handle
+    }
+
+    db.doc(`/users/$`)
+
+
+    firebase.auth().createUserWithEmailAndPassword( newUser.email, newUser.password)
+        .then( data => {
+            return response.status(201).json({ message: `user ${data.user.uid} signed up successfully`})
+        })
+        .catch( err => {
+            console.error(err);
+            return response.status(500).json({ error: err.code })
+        })
+
+})
+
+exports.api = functions.region('asia-northeast1').https.onRequest(app); //
